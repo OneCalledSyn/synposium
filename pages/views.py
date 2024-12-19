49ab5123875c3a6614722b268d55d614cdb78e55
OneCalledSyn <jas2dp@virginia.edu>
2024-12-19 15:26:11 -0500

    Add bootstrap

--- pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-# render() looks for HTML templates inside templates/ directory
-def home(request):
-    return render(request, "pages/home.html", {})
