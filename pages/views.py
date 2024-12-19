f7d43a9042c524a414679943289b890c649b5f9a
OneCalledSyn <jas2dp@virginia.edu>
2024-12-19 02:53:05 -0500

    Created view, added route, fixed syntax in settings

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# render() looks for HTML templates inside templates/ directory
+def home(request):
+    return render(request, "pages/home.html", {})
